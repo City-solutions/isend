b7f080c819d6bd65750fdc56d18043067615b99c
Snr Andy <nfonandrew73@gmail.com>
2025-01-15 10:07:54 +0100

    fix

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,7 +1,7 @@
 // apiService.js
 import axios from 'axios';
 
-const API_URL = 'https://api.isendxpress.com/api';
+const API_URL = process.env.REACT_APP_BACKEND_URL;
 
 export const fetchCategories = async () => {
   try {
--- src/services/paymentService.js
+++ src/services/paymentService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://api.isendxpress.com/api/pay';
+const API_URL = process.env.REACT_APP_BACKEND_URL;
 
 export const sendPaymentRequest = async (payload) => {
   try {
